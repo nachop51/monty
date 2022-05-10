72c809d467e64407fbb134b025788e5a36e3daa6
Nachop51 <nachoperalta0@gmail.com>
2022-05-10 19:23:22 +0000

    Changes before fix

--- bytecodes/000.m
+++ bytecodes/000.m
@@ -1,7 +1,7 @@
 push 0
-push 1
+push -1
 push 2
-push 3
+push -3
 pall
 push 4
 push 5
--- n/nop.c
+++ n/nop.c
@@ -2,7 +2,6 @@
 
 void nop(stack_t **head, unsigned int lineCount)
 {
-	argument = -2;
 	(void)head;
 	(void)lineCount;
 }
